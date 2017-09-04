30f3af201ee2580d5f818de7fd527cb4614a44e9
Entroper <entroper@gmail.com>
2017-09-04 12:51:18 -0400

    FF1R package script -> 2.0

--- FF1R/package.sh
+++ FF1R/package.sh
@@ -1,9 +1,9 @@
 #~/bin/bash
 
-rm bin/Release/netcoreapp1.1/publish/*
+rm bin/Release/netcoreapp2.0/publish/*
 dotnet restore
 dotnet publish -c Release
-pushd bin/Release/netcoreapp1.1/publish
+pushd bin/Release/netcoreapp2.0/publish
 tar czf ff1r.tar.gz *
 mv ff1r.tar.gz ../../../..
 popd
