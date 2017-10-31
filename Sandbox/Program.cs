653935d55724bbf0747bc8ff370243cec0c56b52
Entroper <entroper@gmail.com>
2017-10-31 00:20:10 -0400

    bring back Sandbox

---
+++ Sandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FF1Lib;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        Console.WriteLine(FF1Text.BytesToText(new byte[] { 0x99, 0x98, 0x92, 0x9C, 0x98, 0x97 }));
+	        Console.ReadKey();
+        }
+    }
+}
