30a1b3292acb683ebc2ae2a235b19b31b10d346c
Entroper <entroper@vt.edu>
2017-12-10 00:17:21 -0500

    fix beta deployment

--- FF1RandomizerOnline/deploy.sh
+++ FF1RandomizerOnline/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 branch_name=$(git symbolic-ref --short -q HEAD)
-if [ branch_name == 'dev' ]; then
+if [ $branch_name == 'dev' ]; then
 	dotnet publish -c Debug
-	docker build -t ff1randomizeronline:beta .
+	docker build -t ff1randomizeronline:beta -f Dockerfile.beta .
 	docker stop ff1robeta
 	docker rm ff1robeta
 	docker run -d --name ff1robeta --network web-internal ff1randomizeronline:beta
-elif [ branch_name == 'master' ]; then
+elif [ $branch_name == 'master' ]; then
 	dotnet publish -c Release
 	docker build -t ff1randomizeronline:latest .
 	docker stop ff1ro
