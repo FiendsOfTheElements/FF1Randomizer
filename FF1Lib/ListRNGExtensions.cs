50017b06ed24aa1d27bf68bf344b21f2c304eccb
David Moeller <dhmoeller@gmail.com>
2018-01-20 13:38:58 -0600

    Add ListRNGExtensions

---
+++ FF1Lib/ListRNGExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RomUtilities;
+
+namespace FF1Lib
+{
+    public static class ListRNGExtensions
+    {
+        public static T PickRandom<T>(this IList<T> fromList, MT19337 rng)
+        {
+            return fromList[rng.Between(0, fromList.Count - 1)];
+        }
+        public static T SpliceRandom<T>(this IList<T> fromList, MT19337 rng)
+        {
+            var splice = fromList.PickRandom<T>(rng);
+            fromList.Remove(splice);
+            return splice;
+        }
+        public static T PickRandom<T>(this HashSet<T> fromSet, MT19337 rng)
+        {
+            return fromSet.ToList().PickRandom<T>(rng);
+        }
+        public static T SpliceRandom<T>(this HashSet<T> fromSet, MT19337 rng)
+        {
+            var splice = fromSet.ToList().PickRandom<T>(rng);
+            fromSet.Remove(splice);
+            return splice;
+        }
+    }
+
+}
