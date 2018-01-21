eb5ebd525ce55cc80017797c56e0069d5acaf8fe
Entroper <entroper@gmail.com>
2018-01-20 21:31:12 -0500

    update version to 1.6.3

--- FF1Lib/FF1Rom.cs
+++ FF1Lib/FF1Rom.cs
@@ -12,7 +12,7 @@ namespace FF1Lib
 	// ReSharper disable once InconsistentNaming
 	public partial class FF1Rom : NesRom
 	{
-		public const string Version = "1.6.2";
+		public const string Version = "1.6.3";
 
 		public const int CopyrightOffset1 = 0x384A8;
 		public const int CopyrightOffset2 = 0x384BA;
