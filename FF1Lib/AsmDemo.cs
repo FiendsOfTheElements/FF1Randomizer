d4cb92275e6e2075f66129e69561b773ad8e7970
drcatdoctor <854793+drcatdoctor@users.noreply.github.com>
2018-09-08 21:05:08 +0100

    move the assembler into a NuGet package and use it.

---
+++ FF1Lib/AsmDemo.cs
@@ -0,0 +1,46 @@
+﻿using Patcher6502;
+using System;
+using System.Diagnostics;
+
+namespace FF1Lib
+{
+	partial class FF1Rom
+	{
+		public void DemoPatchAssembler()
+		{
+			string src = @"
+; *victory music*
+
+GoodStuff:
+LDA #$FF
+ADC #1
+SBC #1
+SEC
+BCS Forever
+
+Forever: BCS GoodStuff
+
+NOP ; EA sports
+
+JMP GoodStuff ; just to demo that the address is actually 8000
+";
+			byte[] result = PatchAssembler.Assemble(0x8000, "test code", src);
+
+			string hex = BitConverter.ToString(result).Replace("-", string.Empty);
+
+			Debug.Assert(hex == "A9FF6901E90138B000B0F5EA4C0080");
+
+			// alternatively, if you wanted to make extra sure your code was the right size, you could call
+			byte[] the_same_result = PatchAssembler.AssembleAndAssertSize(0x8000, 15, "test code again", src);
+
+			Debug.Assert(result == the_same_result);
+			Debug.Assert(result.Length == 15);
+
+			Console.WriteLine("result:");
+			Console.WriteLine(hex);
+			Console.WriteLine("--- result finished ---");
+			Console.WriteLine("Press enter to close...");
+			Console.ReadLine();
+		}
+	}
+}
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Security.Cryptography;
 using FF1Lib;
-using FF1Lib.Procgen;
-using RomUtilities;
 
 namespace Sandbox
 {
