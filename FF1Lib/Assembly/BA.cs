34b0ad1305f1988c3fdd88ca9b3a5138d5bc6667
drcatdoctor <854793+drcatdoctor@users.noreply.github.com>
2018-09-09 19:50:22 +0100

    Add all Disch's labels, defines, etc., a BA struct for bank+address, and python tools to generate the symbol classes.

---
+++ FF1Lib/Assembly/BA.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FF1Lib.Assembly
+{
+	/// <summary>
+	/// Bank and Address.
+	/// </summary>
+	public struct BA
+	{
+		// these should be ushorts of course, but it makes everything annoying (casts everywhere)
+		public int bank;
+		public int addr;
+		public BA(int bank, int addr)
+		{
+			this.bank = bank;
+			this.addr = addr;
+		}
+
+		public int MMC3RomLocation()
+		{
+			return bank * 0x4000 + (addr - (bank == 0x1F ? 0xC000 : 0x8000));
+		}
+
+		public static BA FromMMC1RomLocation(int romOffset)
+		{
+			return _fromRomLocation(0x0F, romOffset);
+		}
+
+		public static BA FromMMC3RomLocation(int romOffset)
+		{
+			return _fromRomLocation(0x1F, romOffset);
+		}
+
+		private static BA _fromRomLocation(int lastBank, int romOffset)
+		{
+			int bank, addr;
+			if (romOffset >= 0x7C000)
+			{
+				bank = lastBank;
+				addr = romOffset - (0x7C000 - 0xC000);
+			}
+			else
+			{
+				bank = romOffset / 0x4000;
+				addr = romOffset % 0x4000 + 0x8000;
+			}
+			return new BA(bank, addr);
+		}
+
+	}
+
+}
