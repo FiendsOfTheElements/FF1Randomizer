d9177d2ad7f873d29e19d11dd1dfc9fcf151a630
drcatdoctor <854793+drcatdoctor@users.noreply.github.com>
2018-09-10 22:46:12 +0100

    update symbol data after bank swap & MMC3 change

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -13,10 +13,18 @@ namespace Sandbox
         {
 			var filename = "ff1.nes";
 			var fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+			var rom = new FF1Rom(fs);
+
 
 			rom = new FF1Rom(fs);
 			csharpRNG = RNGCryptoServiceProvider.Create();
-	        TestMapGen.Run(rom, csharpRNG);
+
+	        // asm
+	        //rom.UpgradeToMMC3();
+	        //rom.DemoPatchAssembler();
+
+			// mapgen
+	        //TestMapGen.Run(rom, csharpRNG);
         }
 	}
 }
