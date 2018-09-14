289d515e065f41f6255bcd9badc64075da55fad2
drcatdoctor <854793+drcatdoctor@users.noreply.github.com>
2018-09-14 22:15:34 +0100

    ascii map output for debug

--- FF1Lib/MapGenerator.cs
+++ FF1Lib/MapGenerator.cs
@@ -11,6 +11,98 @@ namespace FF1Lib
 		public Map Map;
 		public Coordinate Entrance;
 		public MapRequirements Requirements;
+
+		static private Dictionary<Tile, char> asciiTileMap = new Dictionary<Tile, char>
+		{
+			{ Tile.RoomBackLeft, '/' },
+			{ Tile.RoomBackCenter, '=' },
+			{ Tile.RoomBackRight, '\\' },
+			{ Tile.RoomLeft, '|' },
+			{ Tile.RoomCenter, '`' },
+			{ Tile.RoomRight, '|' },
+			{ Tile.RoomFrontLeft, '|' },
+			{ Tile.RoomFrontCenter, '|' },
+			{ Tile.RoomFrontRight, '|' },
+			{ Tile.Ladder, '#' },
+			{ Tile.LadderHole, 'o' },
+			{ Tile.WarpUp, '<' },
+			{ Tile.InsideWall, 'x' },
+			{ Tile.FloorSafe, 'f' },
+			{ Tile.Impassable, 'X' },
+			{ Tile.Door, 'D' },
+			{ Tile.Doorway, 'd' },
+			{ Tile.Lava, 'L' },
+			{ Tile.WaterfallInside, '_' },
+			{ Tile.WaterfallRandomEncounters, ' ' },
+		};
+
+		public string AsText()
+		{
+			StringBuilder sb = new StringBuilder(MapRequirements.Height * MapRequirements.Width);
+
+			int y, x;
+			char c;
+			var otherCharMap = new Dictionary<int, char>();
+			var seenValues = new HashSet<int>();
+
+			// other chars to pull from when automatically mapping byte values to chars
+			// just letters obviously. someone smart can come up with a better idea.
+			var otherChars = new Stack<char>("ABCDFGHIJKLMNPRSTUVWYZxabcdefghijklmnopqrstuwvz");
+
+			sb.Append($"Map {Requirements.MapId}\n");
+
+			for (y = 0; y < MapRequirements.Height; y++)
+			{
+				bool seenEntrance = false;
+				for (x = 0; x < MapRequirements.Width; x++)
+				{
+					int value = Map[y, x];
+					if (asciiTileMap.ContainsKey((Tile) value))
+						sb.Append(asciiTileMap[(Tile) value]);
+					else if (otherCharMap.ContainsKey(value))
+						sb.Append(otherCharMap[value]);
+					else
+					{
+						char newchar = otherChars.Pop();
+						otherCharMap.Add(value, newchar);
+						sb.Append(newchar);
+					}
+
+					seenValues.Add(value);
+					if (value == (int) Tile.WarpUp)
+						seenEntrance = true;
+				}
+
+				if (seenEntrance)
+					sb.Append("\t <-- Entrance this row");
+
+				sb.Append("\n");
+			}
+
+			sb.Append("\n");
+
+			// make a nice little table
+			int index = 0;
+			var legend_groups = asciiTileMap.
+				Where(entry => seenValues.Contains((int) entry.Key)).
+				Select(entry => String.Format("{0,-30}  {1,2}", entry.Key, entry.Value)).
+				GroupBy(str => index++ / 2).ToList();
+			legend_groups.ForEach(grouping =>
+				sb.Append("\t").Append(String.Join("\t\t\t", grouping)).Append("\n")
+			);
+			sb.AppendLine();
+
+			index = 0;
+			legend_groups = otherCharMap.
+				Select(entry => $"{entry.Key:X2}        {entry.Value}").
+				GroupBy(str => index++ / 4).ToList();
+			legend_groups.ForEach(grouping =>
+				sb.Append("\t").Append(String.Join("\t\t", grouping)).Append("\n")
+			);
+
+			return sb.ToString();
+		}
+
 	}
 
 	public struct MapRequirements
@@ -37,7 +129,11 @@ namespace FF1Lib
 	{
 		public CompleteMap Generate(MT19337 rng, MapGeneratorStrategy strategy, MapRequirements reqs)
 		{
-			return GetStrategy(strategy).Generate(rng, reqs);
+			var completemap = GetStrategy(strategy).Generate(rng, reqs);
+#if DEBUG
+			Console.Write(completemap.AsText());
+#endif
+			return completemap;
 		}
 
 		private IMapGeneratorEngine GetStrategy(MapGeneratorStrategy strategy)
@@ -73,7 +169,8 @@ namespace FF1Lib
 		{
 			CompleteMap complete = new CompleteMap
 			{
-				Map = new Map((byte)Tile.WaterfallInside)
+				Map = new Map((byte)Tile.WaterfallInside),
+				Requirements = reqs
 			};
 
 			//Map map = complete.Map;
@@ -353,7 +450,8 @@ namespace FF1Lib
 
 				CompleteMap complete = new CompleteMap
 				{
-					Map = new Map((byte)Tile.Lava)
+					Map = new Map((byte)Tile.Lava),
+					Requirements = reqs
 				};
 
 				var room = GenerateTreasureRoom(rng, reqs.Treasures.ToList());
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 using FF1Lib;
 using RomUtilities;
 
@@ -8,7 +12,37 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-	        UsedText.GetUsedText();
-        }
+			var filename = "ff1.nes";
+			var fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+			var rom = new FF1Rom(fs);
+			var provider = RNGCryptoServiceProvider.Create();
+			var seed = new byte[8];
+			MT19337 rng;
+
+			MapRequirements reqs = new MapRequirements
+			{
+				MapId = MapId.Waterfall,
+				Floor = Tile.WaterfallRandomEncounters,
+				Treasures = new List<byte> { 0x79, 0x7A, 0x7B, 0x7C, 0x7D, 0x7E },
+				NPCs = Enumerable.Range(0, 11),
+
+				Rom = rom,
+			};
+
+			MapGenerator generator = new MapGenerator();
+			MapGeneratorStrategy strategy = MapGeneratorStrategy.WaterfallClone;
+
+
+			while (true)
+			{
+				provider.GetBytes(seed);
+				rng = new MT19337(BitConverter.ToUInt32(seed, 0));
+				CompleteMap waterfall = generator.Generate(rng, strategy, reqs);
+				Console.WriteLine("Press a key...");
+				Console.ReadLine();
+			}
+
+		}
 	}
 }
