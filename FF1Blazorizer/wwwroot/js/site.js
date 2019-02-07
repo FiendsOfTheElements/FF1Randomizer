3f42e4efc731079a84f69ecfee8a7eb762720ca6
Mike Lattanzio <latzio@gmail.com>
2019-02-06 23:35:23 -0500

    Make export work. Clean up JS.

--- FF1Blazorizer/wwwroot/js/site.js
+++ FF1Blazorizer/wwwroot/js/site.js
@@ -1,13 +1,13 @@
 ﻿function handleFileSelect(inputId) {
 
-	var input = document.getElementById(inputId);
-	var file = input.files[0];
-	var reader = new FileReader();
+	const input = document.getElementById(inputId);
+	const file = input.files[0];
+	const reader = new FileReader();
 
 	return new Promise((resolve, reject) => {
 
 		reader.onload = e => {
-			let encoded = e.target.result.split(',')[1];
+			const encoded = e.target.result.split(',')[1];
 			resolve(encoded);
 		};
 		reader.onerror = () => {
@@ -23,12 +23,12 @@
 
 function downloadROM(filename, encoded) {
 
-	var url = "data:application/octet-stream;base64," + encoded;
+	const url = "data:application/octet-stream;base64," + encoded;
 	fetch(url)
 		.then(result => result.blob())
 		.then(blob => {
 
-			var anchor = document.createElement('a');
+			const anchor = document.createElement('a');
 
 			anchor.download = filename;
 			anchor.href = window.URL.createObjectURL(blob);
@@ -40,11 +40,20 @@ function downloadROM(filename, encoded) {
 
 function updateHistory(seedString, flagString) {
 
-	var href = document.location.href;
+	let href = document.location.href;
 	if (href.indexOf('?') > 0) {
 		href = href.substr(0, href.indexOf('?'));
 	}
 
 	history.replaceState({}, '', href + '?' + 's=' + seedString + '&' + 'f=' + flagString);
 
+}
+
+function copyLocation() {
+	const textarea = document.createElement('textarea');
+	textarea.value = location.href;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	document.body.removeChild(textarea);
 }
