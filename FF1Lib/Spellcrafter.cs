d68d0c67840b7a40ff6b9ff1767d1dd6cab272c4
one1fineday <50121891+one1fineday@users.noreply.github.com>
2019-08-07 00:18:35 -0400

    Add files via upload

--- FF1Lib/Spellcrafter.cs
+++ FF1Lib/Spellcrafter.cs
@@ -192,7 +192,7 @@ namespace FF1Lib
 			spellNames[selected] = levelShuffle ? "LIF" + (SpellTier(selected) + 1).ToString() : "LIFE";
 			spellMessages[selected] = 0x4A; // Ineffective now
 			SPCR_SetPermissionFalse(spellPermissions, selected, 6); // knight banned
-			SPCR_SetPermissionFalse(spellPermissions, selected, 9); // red wizard banned
+			SPCR_SetPermissionFalse(spellPermissions, selected, 3); // red mage banned
 			Put(MagicOutOfBattleOffset + MagicOutOfBattleSize * 8, new[] { (byte)(selected + 0xB0) });
 			spellindex.Remove(selected);
 			selected = spellindex.Where(id => WhiteSpell(id) && id > 47).ToList().PickRandom(rng);
