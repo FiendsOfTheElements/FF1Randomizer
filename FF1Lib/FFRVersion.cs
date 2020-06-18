3f020a53c0a748b9f2d220065d2c9d6148fc1573
Steven Biro <steven.biro@mail.utoronto.ca>
2020-06-17 23:06:25 -0400

    change the test sha to the placeholder

--- FF1Lib/FFRVersion.cs
+++ FF1Lib/FFRVersion.cs
@@ -3,7 +3,7 @@
 	public static class FFRVersion
 	{
 		public const string Version = "3.1.1";
-		public const string Sha = "284523517707d26fd10cbc053a58d0ffc85be08a";
+		public const string Sha = "SHA";
 		public const string Branch = "alpha";
 		public const string MasterBranch = "master";
 	}
