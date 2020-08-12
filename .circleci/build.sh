de7b6f8d4427ee861eb8e39983a73ebfb2bc798b
Steven Biro <steven.biro@mail.utoronto.ca>
2020-08-12 18:15:27 -0400

    use variables to replace constants

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -9,18 +9,18 @@ cd FF1Blazorizer
 
 config=$(jq -r ".branchConfig | map(select(if .branch == \"default\" then true elif .branch == \"master\" then true else false end)) | .[0]" ../.circleci/configs/config.json)
 cat <<<$config
-longName=$(jq ".longName" <<<"$config")
-shortName=$(jq ".shortName" <<<"$config")
-cssFile=$(jq ".cssFile" <<<"$config")
-themeColor=$(jq ".themeColor" <<<"$config")
-siteIcon=$(jq ".siteIcon" <<<"$config")
-releaseBuild=$(jq ".releaseBuild" <<<"$config")
+longName=$(jq -r ".longName" <<<"$config")
+shortName=$(jq -r ".shortName" <<<"$config")
+cssFile=$(jq -r ".cssFile" <<<"$config")
+themeColor=$(jq -r ".themeColor" <<<"$config")
+siteIcon=$(jq -r ".siteIcon" <<<"$config")
+releaseBuild=$(jq -r ".releaseBuild" <<<"$config")
 
-sed -i 's/NAME_LONG/$longName/g' wwwroot/manifest.published.json
-sed -i 's/NAME_SHORT/FFR/g' wwwroot/manifest.published.json
-sed -i "s/DARK_BACKGROUND_COLOR/$color/g" wwwroot/manifest.published.json
-sed -i "s/DARK_BACKGROUND_COLOR/$color/g" wwwroot/index.html
-sed -i 's/SITE_ICON_COLOR/normal/g' wwwroot/manifest.published.json
-sed -i 's/SITE_ICON_COLOR/normal/g' wwwroot/index.html
+sed -i "s/NAME_LONG/${longName}/g" wwwroot/manifest.published.json
+sed -i "s/NAME_SHORT/$shortName/g" wwwroot/manifest.published.json
+sed -i "s/DARK_BACKGROUND_COLOR/$themeColor/g" wwwroot/manifest.published.json
+sed -i "s/DARK_BACKGROUND_COLOR/$themeColor/g" wwwroot/index.html
+sed -i "s/SITE_ICON_COLOR/$siteIcon/g" wwwroot/manifest.published.json
+sed -i "s/SITE_ICON_COLOR/$siteIcon/g" wwwroot/index.html
 mv -f wwwroot/manifest.published.json wwwroot/manifest.json
 dotnet publish -c Release -o output
