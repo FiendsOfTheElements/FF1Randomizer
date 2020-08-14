ce428fd0082d166f4d1be508d91a844955dee58a
Steven Biro <steven.biro@mail.utoronto.ca>
2020-08-13 20:02:07 -0400

    testing comments on a PR

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,30 @@
-#!/bin/sh 
-if [ "${CIRCLE_BRANCH}" = "master" ]; then
-    netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --prod --site=cd1fef70-df7f-44ab-b1c8-3239dae773ac
-elif [ "${CIRCLE_BRANCH}" = "dev" ]; then
-    netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --prod --site=b1e4219e-dfb6-4efa-924f-3136bbf3fa26
+#!/bin/sh
+config=$(jq -r ".branchConfig | map(select(if .branch == \"default\" then true elif .branch == \"${CIRCLE_BRANCH}\" then true else false end)) | .[0]" .circleci/configs/config.json)
+echo "$config" | cat
+netlifyID=$(echo "$config" | jq -r ".netlifyID")
+deployPreview=$(echo "$config" | jq -r ".deployPreview")
+if "$deployPreview"; then
+    # netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --site="$netlifyID" | grep "Website Draft URL" | cat
+    # netlify deploy --dir=FF1Blazorizer/output/wwwroot --site=4ef4838f-158d-4929-bd24-d516bec18708 --json | jq -r ".deploy_url" | cat
+
+    GH_USER=Testing
+    GH_API=9b7287752289cc259bdf8f720d5a416f3c9984d5
+    pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" \
+        -u $GH_USER:$GH_API)
+
+    if [ $(echo $pr_response | jq length) -eq 0 ]; then
+        echo "No PR found to update"
+    else
+        pr_comment_url=$(echo $pr_response | jq -r ".[]._links.comments.href")
+    fi
+
+    curl --location --request POST "$pr_comment_url" \
+        -u $GH_USER:$GH_API \
+        --header 'Content-Type: application/json' \
+        --data-raw '{
+       "body": "This would be the data to add"
+      }'
+
 else
-    netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --prod --site=61141991-cfa6-4a2e-ab6b-187b1611a2e4
+    # netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --prod --site="$netlifyID"
 fi
