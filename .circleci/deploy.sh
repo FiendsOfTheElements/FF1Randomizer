d84531987f18cfade78f41e27bf6a2c19fb73d7f
Steven Biro <steven.biro@mail.utoronto.ca>
2020-08-15 00:12:24 -0400

    git shell errors

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -11,15 +11,15 @@ if "$deployPreview"; then
     # netlify deploy --dir=FF1Blazorizer/output/wwwroot --site=4ef4838f-158d-4929-bd24-d516bec18708 --json | jq -r ".deploy_url" | cat
 
     GH_USER=FFR_Build_And_Deploy
-    pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" -u $GH_USER:$GH_API)
+    pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" -u $GH_USER:"$GH_API")
 
     if [ $(echo $pr_response | jq length) -eq 0 ]; then
         echo "No PR found to update"
     else
-        pr_comment_url=$(echo $pr_response | jq -r ".[]._links.comments.href")
+        pr_comment_url=$(echo "$pr_response" | jq -r ".[]._links.comments.href")
     fi
 
-    curl -X POST -H "Accept: application/vnd.github.v3+json" "$pr_comment_url" -u $GH_USER:$GH_API -d "{\"body\": \"Automatic deployment: ${url}\"}" | cat
+    curl -X POST -H "Accept: application/vnd.github.v3+json" "$pr_comment_url" -u $GH_USER:"$GH_API" -d "{\"body\": \"Automatic deployment: ${url}\"}" | cat
 
 else
     echo "nothing"
