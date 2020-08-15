b46450ce29f496d5d74559abedd9233e9e48886b
Steven Biro <steven.biro@mail.utoronto.ca>
2020-08-15 13:34:34 -0400

    script seems to be working locally now

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,8 +7,7 @@ echo "$config" | cat
 netlifyID=$(echo "$config" | jq -r ".netlifyID")
 deployPreview=$(echo "$config" | jq -r ".deployPreview")
 if "$deployPreview"; then
-    url=$(netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --site="$netlifyID" | grep "Website Draft URL")
-    # netlify deploy --dir=FF1Blazorizer/output/wwwroot --site=4ef4838f-158d-4929-bd24-d516bec18708 --json | jq -r ".deploy_url" | cat
+    url=$(netlify deploy --json --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --site="$netlifyID" | js -r ".deploy_url")
 
     GH_USER=FFR_Build_And_Deploy
     pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" -u $GH_USER:"$GH_API")
@@ -18,8 +17,8 @@ if "$deployPreview"; then
     else
         pr_comment_url=$(echo "$pr_response" | jq -r ".[]._links.comments.href")
     fi
-    echo "{\"body\": \"Automatic deployment: ${url}\"}"
-    curl -X POST -H "Accept: application/vnd.github.v3+json" "$pr_comment_url" -u $GH_USER:"$GH_API" --data-urlencode "{\"body\": \"Automatic deployment: ${url}\"}" | cat
+    post_data='{"body": "Automatic deployment: '$url'"}'
+    curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Content-Type:application/json" "$pr_comment_url" -u $GH_USER:"$GH_API" -d "$post_data"
 
 else
     echo "nothing"
