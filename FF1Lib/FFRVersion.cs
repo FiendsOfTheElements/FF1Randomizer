cbd3887eef4efca43b90fb49dbaeb78bf8c5b5d7
Mike Lattanzio <latzio@gmail.com>
2020-08-22 19:01:43 -0400

    Update branch to sentinel value and add comment.

--- FF1Lib/FFRVersion.cs
+++ FF1Lib/FFRVersion.cs
@@ -1,10 +1,11 @@
 ﻿namespace FF1Lib
 {
+	// THIS FILE IS UPDATED DURING THE BUILD AND DEPLOY. DO NOT EDIT UNLESS YOU KONW WHAT YOU ARE DOING.
 	public static class FFRVersion
 	{
 		public const string Version = "3.1.1";
 		public const string Sha = "SHA";
-		public const string Branch = "alpha";
+		public const string Branch = "BRANCH";
 		public const string MasterBranch = "master";
 	}
 }
