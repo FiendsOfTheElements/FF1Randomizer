5795bac865d1cd723092cbc0ec8b968f504b0179
Steven Biro <steven.biro@mail.utoronto.ca>
2020-11-25 23:27:48 -0500

    temp commit to test versioned deployment

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,27 +3,28 @@
 
 config=$(jq -r ".branchConfig | map(select(if .branch == \"default\" then true elif .branch == \"${CIRCLE_BRANCH}\" then true else false end)) | .[0]" .circleci/configs/config.json)
 netlifyID=$(echo "$config" | jq -r ".netlifyID")
-deployPreview=$(echo "$config" | jq -r ".deployPreview")
+#deployPreview=$(echo "$config" | jq -r ".deployPreview")
 
 
-if "$deployPreview"; then
-    deploy_response=$(netlify deploy --json --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --site="$netlifyID")
-    url=$(echo "$deploy_response" | jq -r ".deploy_url")
-
-    GH_USER=FFR_Build_And_Deploy
-    pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" -u $GH_USER:"$GH_API")
-
-    if [ "$(echo "$pr_response" | jq length)" -eq 0 ]; then
-        echo "No PR found to update"
-        exit 0
-    else
-        pr_comment_url=$(echo "$pr_response" | jq -r ".[]._links.comments.href")
-    fi
-    post_data='{"body": "Automatic deployment: '$url'"}'
-    curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Content-Type:application/json" "$pr_comment_url" -u $GH_USER:"$GH_API" -d "$post_data"
-
-else
-    version=$(grep " Version.*" /root/ff1randomizer/FF1Lib/FFRVersion.cs | grep -Eo "[0-9\.]+" | tr '.' '-')
+#if "$deployPreview"; then
+#    deploy_response=$(netlify deploy --json --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --site="$netlifyID")
+#    url=$(echo "$deploy_response" | jq -r ".deploy_url")
+#
+#    GH_USER=FFR_Build_And_Deploy
+#    pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" -u $GH_USER:"$GH_API")
+#
+#    if [ "$(echo "$pr_response" | jq length)" -eq 0 ]; then
+#        echo "No PR found to update"
+#        exit 0
+#    else
+#        pr_comment_url=$(echo "$pr_response" | jq -r ".[]._links.comments.href")
+#    fi
+#    post_data='{"body": "Automatic deployment: '$url'"}'
+#    curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Content-Type:application/json" "$pr_comment_url" -u $GH_USER:"$GH_API" -d "$post_data"
+#
+#else
+    #version=$(grep " Version.*" /root/ff1randomizer/FF1Lib/FFRVersion.cs | grep -Eo "[0-9\.]+" | tr '.' '-')
+    version="fake-version"
     siteExists=$(curl --location --request GET 'https://api.netlify.com/api/v1/dns_zones/finalfantasyrandomizer_com/dns_records' \
     --header "Authorization: Bearer ${NETLIFY_TOKEN}" \
     --header 'Content-Type: application/json' | jq -r ".[].hostname" | grep -q "${version}" && echo true || echo false
@@ -49,8 +50,9 @@ else
     fi
     
     id=$(echo "$createdSite" | jq -r ".site_id")
+    echo "$id"
     
     netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --prod --site="$id"
     netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --prod --site="$netlifyID"
 
-fi
+#fi
