2b1df4609ca9f948be0ec2399792afee3ad982f2
Steven Biro <steven.biro@mail.utoronto.ca>
2020-11-29 12:07:09 -0500

    undo the debug stuff

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,38 +3,33 @@ set -e
 set -x
 
 
-#config=$(jq -r ".branchConfig | map(select(if .branch == \"default\" then true elif .branch == \"${CIRCLE_BRANCH}\" then true else false end)) | .[0]" .circleci/configs/config.json)
-#netlifyID=$(echo "$config" | jq -r ".netlifyID")
-#deployPreview=$(echo "$config" | jq -r ".deployPreview")
+config=$(jq -r ".branchConfig | map(select(if .branch == \"default\" then true elif .branch == \"${CIRCLE_BRANCH}\" then true else false end)) | .[0]" .circleci/configs/config.json)
+netlifyID=$(echo "$config" | jq -r ".netlifyID")
+deployPreview=$(echo "$config" | jq -r ".deployPreview")
 
 
-#if "$deployPreview"; then
-#    deploy_response=$(netlify deploy --json --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --site="$netlifyID")
-#    url=$(echo "$deploy_response" | jq -r ".deploy_url")
-#
-#    GH_USER=FFR_Build_And_Deploy
-#    pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" -u $GH_USER:"$GH_API")
-#
-#    if [ "$(echo "$pr_response" | jq length)" -eq 0 ]; then
-#        echo "No PR found to update"
-#        exit 0
-#    else
-#        pr_comment_url=$(echo "$pr_response" | jq -r ".[]._links.comments.href")
-#    fi
-#    post_data='{"body": "Automatic deployment: '$url'"}'
-#    curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Content-Type:application/json" "$pr_comment_url" -u $GH_USER:"$GH_API" -d "$post_data"
-#
-#else
-    #version=$(grep " Version.*" /root/ff1randomizer/FF1Lib/FFRVersion.cs | grep -Eo "[0-9\.]+" | tr '.' '-')
-    version="fake-version6"
+if "$deployPreview"; then
+    deploy_response=$(netlify deploy --json --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --site="$netlifyID")
+    url=$(echo "$deploy_response" | jq -r ".deploy_url")
+
+    GH_USER=FFR_Build_And_Deploy
+    pr_response=$(curl --location --request GET "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open" -u $GH_USER:"$GH_API")
+
+    if [ "$(echo "$pr_response" | jq length)" -eq 0 ]; then
+        echo "No PR found to update"
+        exit 0
+    else
+        pr_comment_url=$(echo "$pr_response" | jq -r ".[]._links.comments.href")
+    fi
+    post_data='{"body": "Automatic deployment: '$url'"}'
+    curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Content-Type:application/json" "$pr_comment_url" -u $GH_USER:"$GH_API" -d "$post_data"
+
+else
+    version=$(grep " Version.*" /root/ff1randomizer/FF1Lib/FFRVersion.cs | grep -Eo "[0-9\.]+" | tr '.' '-')
     siteExists=$(curl --location --request GET 'https://api.netlify.com/api/v1/dns_zones/finalfantasyrandomizer_com/dns_records' \
     --header "Authorization: Bearer ${NETLIFY_AUTH_TOKEN}" \
     --header 'Content-Type: application/json' | jq -r ".[].hostname" | grep -q "${version}" && echo true || echo false
     )
-    #siteExists=$(curl --location --request GET 'https://api.netlify.com/api/v1/dns_zones/stevenbir_com/dns_records' \
-    #--header "Authorization: Bearer ${NETLIFY_AUTH_TOKEN}" \
-    #--header 'Content-Type: application/json' | jq -r ".[].hostname" | grep -q "${version}" && echo true || echo false
-    #)
     
     
     if [ "${siteExists}" = true ]; then
@@ -42,10 +37,6 @@ set -x
     	exit 1
     fi
     
-    #createdSite=$(curl --location --request POST 'https://api.netlify.com/api/v1/sites' \
-    #--header "Authorization: Bearer ${NETLIFY_AUTH_TOKEN}" \
-    #--header 'Content-Type: application/json' \
-    #--data-raw "{\"custom_domain\": \"${version}.stevenbiro.com\", \"force_ssl\": \"true\"}")
     createdSite=$(curl --location --request POST 'https://api.netlify.com/api/v1/sites' \
     --header "Authorization: Bearer ${NETLIFY_AUTH_TOKEN}" \
     --header 'Content-Type: application/json' \
@@ -63,6 +54,6 @@ set -x
     echo "$id"
     
     netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --prod --site="$id"
-    #netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --prod --site="$netlifyID"
+    netlify deploy --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --prod --site="$netlifyID"
 
-#fi
+fi
