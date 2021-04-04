c072920a2272b64ce89f3e91107c7d946e70553e
wildham0 <64616385+wildham0@users.noreply.github.com>
2021-04-04 13:01:29 -0400

    No Overworld Mode (#570)
    Add a "No Overworld" option to SanityV2, merging all dungeons, towns and castle in one continuous dungeon.

--- FF1Lib/FlagsViewModel.cs
+++ FF1Lib/FlagsViewModel.cs
@@ -1664,32 +1664,41 @@ namespace FF1Lib
 				Flags.FreeBridge = value;
 				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FreeBridge"));
 			}
-		}
+		}/*
 		public bool? FreeShip
 		{
 			get => Flags.FreeShip;
 			set
 			{
 				Flags.FreeShip = value;
-				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FreeAirship"));
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FreeShip"));
 			}
-		}
-		public bool? FreeAirship
+		}*/
+		public bool? FreeShipFlag
 		{
-			get => Flags.FreeAirship;
+			get => Flags.FreeShipFlag;
 			set
 			{
-				Flags.FreeAirship = value;
-				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FreeAirship"));
+				Flags.FreeShipFlag = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FreeShipFlag"));
 			}
 		}
-		public bool? FreeCanal
+		public bool? FreeAirshipFlag
 		{
-			get => Flags.FreeCanal;
+			get => Flags.FreeAirshipFlag;
 			set
 			{
-				Flags.FreeCanal = value;
-				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FreeCanal"));
+				Flags.FreeAirshipFlag = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FreeAirshipFlag"));
+			}
+		}
+		public bool? FreeCanalFlag
+		{
+			get => Flags.FreeCanalFlag;
+			set
+			{
+				Flags.FreeCanalFlag = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FreeCanalFlag"));
 			}
 		}
 		public bool? FreeCanoe
@@ -4123,7 +4132,7 @@ namespace FF1Lib
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SpoilerBatsDontCheckOrbs"));
 		    }
 		}
-    
+
 		public bool SanityCheckerV2
 		{
 			get => Flags.SanityCheckerV2;
