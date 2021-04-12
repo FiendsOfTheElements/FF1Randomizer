8ede54d10d2d75f6c62bebcc2b0194dc8abc002b
HQS <highqualityshaun@gmail.com>
2021-04-11 23:10:01 -0400

    disable early spoilers unless hints enabled; tooltips (#582)

--- FF1Lib/FlagsViewModel.cs
+++ FF1Lib/FlagsViewModel.cs
@@ -4215,6 +4215,8 @@ namespace FF1Lib
 		    }
 		}
 
+		public bool SpoilerBatsDontCheckOrbsEnabled => !SkyWarriorSpoilerBats.Equals(SpoilerBatHints.Vanilla);
+
 		public bool SanityCheckerV2
 		{
 			get => Flags.SanityCheckerV2;
