9b2b912798523c514bc6a474307e227072263b89
Peter Amstutz <peter.amstutz@curii.com>
2021-08-22 15:47:48 -0400

    Procgen framework functioning end-to-end.

--- FF1R/Commands/Procgen.cs
+++ FF1R/Commands/Procgen.cs
@@ -3,7 +3,8 @@ namespace FF1R.Commands
 	using System;
 	using System.ComponentModel.DataAnnotations;
 	using McMaster.Extensions.CommandLineUtils;
-
+	using RomUtilities;
+	using System.IO;
 	using FFR.Common;
 
 	[Command("procgen", Description = "Create a procedurally generated map")]
@@ -15,8 +16,16 @@ namespace FF1R.Commands
 
 		int OnExecute(IConsole console)
 		{
-			console.Error.WriteLine($"procgen subcommand {OutputFile}");
-			return 1;
+			var rng = new MT19337(125);
+			var replacement = FF1Lib.Procgen.NewOverworld.GenerateNewOverworld(rng);
+			using (BinaryWriter writer = new BinaryWriter(File.Open("map5.ffm", FileMode.Create))) {
+				for (int y = 0; y < FF1Lib.Procgen.OverworldState.MAPSIZE; y++) {
+					for (int x = 0; x < FF1Lib.Procgen.OverworldState.MAPSIZE; x++) {
+						writer.Write(replacement.Tiles[y][x]);
+					}
+				}            
+        	}
+			return 0;
 		}
 	}
 }
