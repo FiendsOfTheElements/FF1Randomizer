6715ae09f90ce8c1876f767099b57c914b9c5895
Peter Amstutz <peter.amstutz@curii.com>
2021-08-31 22:49:18 -0400

    emit json

--- FF1R/Commands/Procgen.cs
+++ FF1R/Commands/Procgen.cs
@@ -6,9 +6,10 @@ namespace FF1R.Commands
 	using RomUtilities;
 	using System.IO;
 	using FFR.Common;
+	using Newtonsoft.Json;
 
 	[Command("procgen", Description = "Create a procedurally generated map")]
-	
+
 	class Procgen
 	{
 		[Argument(0, Description = "The output file name")]
@@ -16,16 +17,21 @@ namespace FF1R.Commands
 
 		int OnExecute(IConsole console)
 		{
-			var rng = new MT19337(125);
-			var replacement = FF1Lib.Procgen.NewOverworld.GenerateNewOverworld(rng);
-			using (BinaryWriter writer = new BinaryWriter(File.Open("map5.ffm", FileMode.Create))) {
-				for (int y = 0; y < FF1Lib.Procgen.OverworldState.MAPSIZE; y++) {
-					for (int x = 0; x < FF1Lib.Procgen.OverworldState.MAPSIZE; x++) {
-						writer.Write(replacement.Tiles[y][x]);
-					}
-				}            
-        	}
-			return 0;
+		    var rng = new MT19337(125);
+		    var replacement = FF1Lib.Procgen.NewOverworld.GenerateNewOverworld(rng);
+		    using (BinaryWriter writer = new BinaryWriter(File.Open("procgen_map.ffm", FileMode.Create))) {
+			for (int y = 0; y < FF1Lib.Procgen.OverworldState.MAPSIZE; y++) {
+			    for (int x = 0; x < FF1Lib.Procgen.OverworldState.MAPSIZE; x++) {
+				writer.Write(replacement.Tiles[y][x]);
+			    }
+			}
+		    }
+		    using (StreamWriter file = File.CreateText("procgen_map.json")) {
+			JsonSerializer serializer = new JsonSerializer();
+			serializer.Formatting = Formatting.Indented;
+			serializer.Serialize(file, replacement.ExchangeData);
+		    }
+		    return 0;
 		}
 	}
 }
