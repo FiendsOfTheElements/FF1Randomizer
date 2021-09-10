d686080f4826ffdb65ddb64a9f01b39493f99daf
x.y <x.y@>
2021-09-10 10:46:05 +0200

    comments

--- FF1Lib/PredictivePlacement.cs
+++ FF1Lib/PredictivePlacement.cs
@@ -14,6 +14,14 @@ namespace FF1Lib
 		static HashSet<Item> GatingItems = new HashSet<Item> { Item.Crown, Item.Key, Item.Lute, Item.Rod, Item.Cube, Item.Oxyale, Item.Tnt };
 		static HashSet<Item> KeyItems = new HashSet<Item>(MapChangeItems.Concat(FetchQuestItems).Concat(GatingItems));
 
+		//Thy algorithm sllows flexible placement of many items. The average order is set by 3 Factors.
+		//A chance modifier(ExceptionWeights)
+		//A cycle based restriction(ExceptionMinCycled
+		//A restriction based on how many RewardSources are availble
+
+		//The ExceptionWeights directly modify the chance items will be picked when they are ellegible by the algorithm
+		//The bridge should be earlier, so it's given a high weight. The floater should be late so it'S given a low weight.
+		//This system explicitly allows the bridge to be placed after the floater, it just makes it less likely
 		Dictionary<Item, float> SafeExceptionWeights = new Dictionary<Item, float>
 		{
 			{ Item.Bridge, 2.0f },
@@ -25,6 +33,10 @@ namespace FF1Lib
 			{ Item.Oxyale, 0.25f }
 		};
 
+		//Each cycle the algorithm tries to place an item. It randomly(as long as it works) decides each cycle to place either an incentive or a nonincetive item.
+		//It may be that it doesn't find an item of the chosen type that continues the seed(e.g. there isn't any incentive that opens up the seed anymore)
+		//Then it doesn't place anything in that cycle. If it strictly need the floater to continue, it wastes cycles until the Floater becomes ellegible(cycle 10)
+		//So as long as it finds a way forward, the floater will be late, but theoretically it still could be the first item it places.
 		Dictionary<Item, int> SafeExceptioMinCycles = new Dictionary<Item, int>
 		{
 			{ Item.Key, 5 },
@@ -36,6 +48,9 @@ namespace FF1Lib
 			{ Item.Oxyale, 5 }
 		};
 
+		//This is the only hard restriction. If there's less than 50 RewardSources accessible, the Oxyale may not be placed.
+		//The Chime, Cube, Oxyale aren't that important to continue the seed(there's stil the ship, the canoe, the key, the floater...) so it doesn't create problems.
+		//But it's a safeguard, that the algorithm doesn't force you into sky at the beginning of the game.
 		Dictionary<Item, int> SafeMinAccessibility = new Dictionary<Item, int>
 		{
 			{ Item.Chime, 60 },
@@ -43,6 +58,8 @@ namespace FF1Lib
 			{ Item.Oxyale, 50 }
 		};
 
+		//Same as above, but used with "Allow Unsafe placement"
+		//Doesn't place as high restrictions on the Chime, Cube and Oxyale
 		Dictionary<Item, float> UnsafeExceptionWeights = new Dictionary<Item, float>
 		{
 			{ Item.Bridge, 2.0f },
@@ -72,9 +89,17 @@ namespace FF1Lib
 			{ Item.Oxyale, 5 }
 		};
 
+		//Incentivized Equipment should be late in the game, as such it has a lower chance and can only be placed at cycle 10 or higher.
 		private float NonKeyItemWeight = 0.5f;
 		private float NonKeyItemMinCycle = 10;
 
+		//That's the thresholds the algorithm uses to ensure it always has a way forward(hopefully)
+		//If there are 2 or less incentive locations available it's has to be careful not to waste any incentive locations
+		//The reason for 2 is, that when you have the canoe and still are in the inner sea, neither the ship, nor the canal opens something up by themselves.
+		//It requires both to continue. If it always has 2 incentive locations availble, it can always place both ship and canal and therefore open up the seed.
+		//If there are more than 3 incentive locations available, it's in safe mode(incentive wise) and is happy to place duds(for variation)
+		//but less likely to place items that open up the seed even further
+		//In between is somewhere in between(chance wise)
 		private int CriticalNonIncentiveSourceCount = 5;
 		private int SafeNonIncentiveSourceCount = 20;
 		private int CriticalIncentiveSourceCount = 2;
@@ -111,7 +136,7 @@ namespace FF1Lib
 
 				_sanityCounter++;
 				if (_sanityCounter > 2) throw new InsaneException("Item Placement could not meet incentivization requirements!");
-
+				
 				placedItems = ctx.Forced.ToList();
 				var incentives = new HashSet<Item>(ctx.Incentivized);
 				var nonincentives = ctx.Unincentivized.ToList();
@@ -134,20 +159,28 @@ namespace FF1Lib
 						allKeyItems.Remove(Item.Floater);
 					}
 
+					//The sanity checker currently doesn't allow tracking which shops are available
+					//It could be easily added, but a little randomnes can't hurt(or so I'm thinking)
+					//So it places the vendoritem upfront.
 					var itemShopItem = SelectVendorItem(incentives.ToList(), nonincentives, treasurePool, incentiveLocationPool, rng);
 					placedItems.Add(new ItemShopSlot(_caravanItemLocation, itemShopItem));
 
 					allPlacements.Remove(itemShopItem);
 					allKeyItems.Remove(itemShopItem);
 
+					//Since it can waste cycles, this number needs to be high enough to be able to place all incentives and all nonincentives and still have some leeway
+					//When it's done it breaks anyway
+					//When it reached enough cycles so no cycle based restricitons apply and can place neither an incentive nor a nonincentive it also breaks
 					for (int cycle = 1; cycle <= 100; cycle++)
 					{
 						var (_, mapLocations, requirements) = _checker.CheckSanity(placedItems, null, _flags);
 						var accessibleSources = GetAllAccessibleRewardSources(preBlackOrbLocationPool, placedItems);
 
+						//look how many rewardsources of each type are available
 						var incSourceCount = accessibleSources.Where(s => incentiveLocationPool.Contains(s)).Count();
 						var nonSourceCount = accessibleSources.Count - incSourceCount;
 
+						//get an accessible rewardsource to give the sanity checker the next item for prediction
 						var testSource = accessibleSources.FirstOrDefault();
 						if (testSource == null)
 						{
@@ -157,6 +190,7 @@ namespace FF1Lib
 
 						List<(Item item, int incCount, int nonCount)> candidates = new List<(Item item, int incCount, int nonCount)>();
 
+						//go through each KI, place it in the testRewardSource, run the sanity checker and see how many RewardSources are available afterwards.
 						foreach (var item in allKeyItems)
 						{
 							placedItems.Add(NewItemPlacement(testSource, item));
@@ -169,12 +203,17 @@ namespace FF1Lib
 							var nonCount = accessibleSources2.Count - incCount - nonSourceCount;
 							incCount -= incSourceCount;
 
+							//if the placed item is an incentive, it will take up an incentive location when it will be placed
+							//and vice versa
 							if (incentives.Contains(item)) incCount--; 
 							if (!incentives.Contains(item)) nonCount--;
 
 							candidates.Add((item, incCount, nonCount));
 						}
 
+						//Since it can happen, that neither the ship, nor the canal by themselves open up something, but the combination does,
+						//here the combination is checked
+						//it grabs another Rewardsource, places Ship and Canal, runs the SanityChecker and calculates the results
 						if (allKeyItems.Contains(Item.Ship) && allKeyItems.Contains(Item.Canal))
 						{
 							var testSource2 = accessibleSources.LastOrDefault();
@@ -202,12 +241,16 @@ namespace FF1Lib
 							if (incentives.Contains(Item.Canal)) incCount--;
 							if (!incentives.Contains(Item.Canal)) nonCount--;
 
+							//it gets the ship and canal candidate from the list, later it's put back in
 							var shipCandidate = candidates.FirstOrDefault(c => c.item == Item.Ship);
 							var canalCandidate = candidates.FirstOrDefault(c => c.item == Item.Canal);
 
 							candidates.Remove(shipCandidate);
 							candidates.Remove(canalCandidate);
 
+							//half of the opened up incentive locations and nonincentive locations hould go to either item(rounded down)
+							//In order to continue the seed, Ship and Canal must open up 2 incentive locations, since they may take up 2
+							//it's not an exact estimate, since it doesn'T differentiate between them beeing incentive or nonincentive items
 							incCount /= 2;
 							nonCount /= 2;
 
@@ -234,51 +277,89 @@ namespace FF1Lib
 
 						var incItemCount = allPlacements.Where(i => incentives.Contains(i)).Count();
 
+						//here it randomly decides whether to place and incentive or a non incentive
+						//the chance is based on the number of incentive and nonincentive items
+						//If however it couldn't place an incentive item in the last cycle, it'll always try a nonincentive this time
+						//and vice versa
 						if (state == PlacementState.Incentive || state == PlacementState.Normal && rng.Between(1, allPlacements.Count) <= incItemCount)
 						{
+							//Filter out nonincentive items
 							candidates = candidates.Where(c => incentives.Contains(c.item)).ToList();
 							var nonKeyCandidates = allPlacements.Where(i => !allKeyItems.Contains(i) && incentives.Contains(i));
 
-							//place incentive
 							if (incSourceCount <= CriticalIncentiveSourceCount)
 							{
+								//we are in incentive critical mode
+								//the function takes weights for different categories of items(depending of how they're expected they change the amount of incentive locations available)
+								//bin0 doesn't open up a new location(it therefore reduces the incentive location count by one)
+								//bin1 opens up 1 location and therefore keeps the count the same
+								//bin2 opens up 2 locations and therefore increases the amount by 1
+								//bin3 opens up more than two locations. It has two parameters here, more on that further down
+
+								//Anyway, we can't afford to reduce the incentive location count here(bin0)
+								//We also don't neccesarily want to keep it the same, but that's okish(bin1)
+								//we want to increase the amount(bin2, bin3)
 								weightedCandidates = AssignWeights(candidates, 0.0f, 0.5f, 1.0f, 1.0f, 0.5f, cycle, accessibleSources.Count);
 
 							}
 							else if (incSourceCount <= SafeIncentiveSourceCount)
 							{
+								//We have 1 incentive location to squander, not a lot, but sufficient
+								//We don't want to reduce it or keep it the same, but it would still be ok(bin0, bin1)
+								//We'd like to increase it a little(bin2)
+								//but not too much(bin3)
 								weightedCandidates = AssignWeights(candidates, 0.5f, 0.5f, 1.0f, 0.5f, 0.25f, cycle, accessibleSources.Count);
+
+								//it's also ok to place a NonKeyItem, wa have an incentive location to squander after all
+								//but don't make it too likely
 								weightedCandidates.AddRange(nonKeyCandidates.Select(i => (GetItemWeight(i, 0.5f, cycle, accessibleSources.Count), i)));
 							}
 							else
 							{
+								//We have at least 2 incentive locations to squander(considered a lot)
+								//We like reducing it by one(for variety)
+								//We like keeping it the same(for variety)
+								//We also like to increase it slightly(for variety)
+								//But we don't like to increase it a lot, but then again it's not a problem, so make it less likely
 								weightedCandidates = AssignWeights(candidates, 1.0f, 1.0f, 1.0f, 0.25f, 0.25f, cycle, accessibleSources.Count);
+
+								//Also we like placing NoneKeyItems
 								weightedCandidates.AddRange(nonKeyCandidates.Select(i => (GetItemWeight(i, 1.0f, cycle, accessibleSources.Count), i)));
 							}
 
+							//If we didn't find any candidates, but we have enough locations to place all remaining items, then we just do that
+							//Otherwise the algorithm would refuse to give up the last 2 incentive locations
 							if (weightedCandidates.Sum(i => i.weight) == 0 && incItemCount <= incSourceCount)
 							{
 								weightedCandidates.AddRange(allPlacements.Where(i => incentives.Contains(i)).Select(i => (1.0f, i)));
 							}
 
+							//If we don't have any candidates and not enough incentive locations to place all, we can't place an incentive in this cycle
 							if (weightedCandidates.Sum(i => i.weight) == 0)
 							{
+								//If it's at least cycle 15 and therefore no cycle based restricctions apply
+								//And in the last cycle we tried to place a NonIncentive
+								//we had all KI options available, but couldn't place any of them, so we're doomed
 								if (cycle >= 15 && state != PlacementState.Normal)
 								{
 									placementFailed = true;
 									break;
 								}
 
+								//since we couldn't place an incentive, we won't be able to do so next cycle(same results), so we don't try to do that next cycle
+								//This heavily speeds up the situations when there are very few loose(low chance of trying to place a loose), but a loose is required to go on.
 								state = PlacementState.NonIncentice;
 								continue;
 							}
 						}
 						else
 						{
+							//same as above, but for nonincentive items.
+							//The main difference is that the RewardSource thresholds are way higher
+							//While it's not a safety concern, cou generally want a lot more chest's available than neccesary to be in the confort zone
 							candidates = candidates.Where(c => !incentives.Contains(c.item)).ToList();
 							var nonKeyCandidates = allPlacements.Where(i => !allKeyItems.Contains(i) && !incentives.Contains(i));
 
-							//place nonincentive
 							if (nonSourceCount <= CriticalNonIncentiveSourceCount)
 							{
 								weightedCandidates = AssignWeights2(candidates, 0.0f, 0.5f, 1.0f, 1.0f, 0.5f, cycle, accessibleSources.Count);
@@ -307,10 +388,22 @@ namespace FF1Lib
 							}
 						}
 
+						//The above weighting strategy works with this mindset:
+						//If it's an all incentive seed, we just need to manage the incentive locations
+						//If it's an all loose seed, we just need to manage the nonincentive locations
+						//If it's something in between, we switch between those two ideas.
+						//It works, because a nonincentive item goes into a nonincentive chest and can't negatively impact the incentive locations.
+						//and vice versa.
+						//There is a flaw in this simplistic approach. When placing a nonincentive, it could additionally weigh in if it's desired for incentive placment.
+						//But that's a thing for another time to think through
 
+						//remove the candidates with weight 0(should be unneccesary, but anyway)
 						weightedCandidates = weightedCandidates.Where(w => w.weight > 0).ToList();
+
+						//shuffle the list(should be totally unneccesary, but it makes me more comfortable)
 						weightedCandidates.Shuffle(rng);
 
+						//randomly select an item from the weighted list
 						Item nextPlacment = Item.None;
 
 						float sum = weightedCandidates.Sum(i => i.weight);
@@ -328,11 +421,15 @@ namespace FF1Lib
 							}
 						}
 
+						//if we didn't find one, just select a random one(this shouldn't happen, but safe is safe)
 						if (nextPlacment == Item.None) nextPlacment = weightedCandidates.PickRandom(rng).item;
 
+						//place the item(same code as in GuidedPlacement to have compatibility with other flags)
 						if (incentives.Contains(nextPlacment))
 						{
 							var rewardSources = accessibleSources.Where(s => incentiveLocationPool.Contains(s)).ToList();
+
+							//can't happen per definition. We already know, that there is a location available for the item(but in case anything unexpectedly goes wrong sure)
 							if (rewardSources.Count == 0) continue;
 
 							var rewardSource = balancedPicker.Pick(rewardSources, false, _flags.LooseItemsSpreadPlacement, false, rng);
@@ -347,20 +444,25 @@ namespace FF1Lib
 							placedItems.Add(NewItemPlacement(rewardSource, nextPlacment));
 						}
 
+						//remove the item from the lists of items to place
 						allPlacements.Remove(nextPlacment);
 						allKeyItems.Remove(nextPlacment);
 
+						//we placed an item so we should randomly select incentive/nonincentive next cycle
 						state = PlacementState.Normal;
 
+						//we're finished
 						if (allPlacements.Count == 0) break;
 					}
 
+					//we couldn't place all items or encountered a placement problem
 					if (allPlacements.Count > 0 || placementFailed)
 					{
 						continue;
 					}
 				}
 
+				//place the shards(almost same code as in GuidedPlacement)
 				if (!placementFailed && shards.Count > 0)
 				{
 					var leftoverItemLocations = GetAllAccessibleRewardSources(preBlackOrbLocationPool, placedItems);
@@ -377,11 +479,13 @@ namespace FF1Lib
 					}
 				}
 
+			//finally check the placement(if we arrive here, it's safe, but let's do it anyway in case something goes wrong)
 			} while (placementFailed || !_checker.CheckSanity(placedItems, null, _flags).Complete);
 
 			return new ItemPlacementResult { PlacedItems = placedItems, RemainingTreasures = treasurePool };
 		}
 
+		//retrieve all accessible RewardSources(with no item in them) from the SanityChecker
 		private List<IRewardSource> GetAllAccessibleRewardSources(IEnumerable<IRewardSource> preBlackOrbLocationPool, List<IRewardSource> placedItems)
 		{
 			return preBlackOrbLocationPool.Where(x => !placedItems.Any(y => y.Address == x.Address)
@@ -389,14 +493,25 @@ namespace FF1Lib
 							.ToList();
 		}
 
+		//Assign weights to all candidates based on the state of the seed, the restrictions and the bin weights
 		private List<(float weight, Item item)> AssignWeights(List<(Item item, int incCount, int nonCount)> candidates, float bin0, float bin1, float bin2, float bin3, float bin3red, int cycle, int accessibility)
 		{
 			var weightedCandidates = new List<(float weight, Item item)>();
 
+			//bin0 reduces the amount of locations by 1
 			weightedCandidates.AddRange(candidates.Where(c => c.incCount < 0).Select(c => (GetItemWeight(c.item, bin0, cycle, accessibility), c.item)));
+
+			//bin1 keeps it the same
 			weightedCandidates.AddRange(candidates.Where(c => c.incCount == 0).Select(c => (GetItemWeight(c.item, bin1, cycle, accessibility), c.item)));
+
+			//bin2 increases it by one
 			weightedCandidates.AddRange(candidates.Where(c => c.incCount == 1).Select(c => (GetItemWeight(c.item, bin2, cycle, accessibility), c.item)));
-			
+
+			//bin3 increases it by more than 1
+			//we would like to weigh the candidates towards items that open up less
+			//we don't want to give you the floater, if the ship would suffice for the moment
+			//the first item in this bin is given tha bin3 weight
+			//subsequent items are given ever smaller fractions of that weight defined by bin3red
 			float weight = bin3;
 			foreach (var c in candidates.Where(c => c.incCount >= 2).OrderBy(c => c.incCount))
 			{
@@ -407,6 +522,10 @@ namespace FF1Lib
 			return weightedCandidates;
 		}
 
+		//this function applies the special rules defined at the beginning of the class
+		//if there aren't enough available RewardSources for this item return a weight of 0
+		//if not enough cycles have passed for this item return a weight of 0
+		//if there is an exceptionweight for this item apply it as a factor
 		private float GetItemWeight(Item item, float weight, int cycle, int accessibility)
 		{
 			if (minAccessibility.TryGetValue(item, out var minAccess) && accessibility < minAccess) return 0.0f;
@@ -419,6 +538,12 @@ namespace FF1Lib
 			return weight;
 		}
 
+		//the same as the one above, but for nonincentives
+		//only difference are the bin thresholds and the variable names
+		//bin0 reduces ocations by 1
+		//bin1 keeps it the same or oncreases it by up to 4
+		//bin2 increases it by up to 19
+		//bin3 increases it by at least 20
 		private List<(float weight, Item item)> AssignWeights2(List<(Item item, int incCount, int nonCount)> candidates, float bin0, float bin1, float bin2, float bin3, float bin3red, int cycle, int accessibility)
 		{
 			var weightedCandidates = new List<(float weight, Item item)>();
