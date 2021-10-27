a6c09648cd929fa0b2b4ca1111043b05150bccf1
iwritecode791 <iwritecode791>
2021-10-27 11:28:30 -0400

    Added fixes for bank location and tooltip.

--- FF1Lib/asm/1C_A200_WMHarmEveryone.asm
+++ FF1Lib/asm/1C_A200_WMHarmEveryone.asm
@@ -1,4 +1,5 @@
-﻿.include "Constants.inc"
+﻿;old length = 21 bytes
+.include "Constants.inc"
 .include "variables.inc"
 
 SwapPRG = $FE03
@@ -10,6 +11,7 @@ BtlMag_PrepHitAndDamage = $B82D
 BtlMag_ApplyDamage = $B8DB
 
 ;modified bank 0C function
+;offset 33915
 .org $B905
 BtlMag_Effect_DamageUndead:
     JSR BtlMag_LoadBaseHitChance        ; Load base hit/crit chance
@@ -17,13 +19,13 @@ BtlMag_Effect_DamageUndead:
     JSR BtlMag_PrepHitAndDamage         ; crit rate, randomize damage
 
     ;put the destination address on the stack for after the bank swap
-    LDA #>(HarmHurtWmOnly-1)
-    PHA
-    LDA #<(HarmHurtWmOnly-1)
-    PHA
+    LDA #>(HarmHurtWmOnly-1) ;2
+    PHA ;1
+    LDA #<(HarmHurtWmOnly-1) ;2
+    PHA ;1
     ;load the new bank
-    LDA #$1B
-    JMP SwapPRG
+    LDA #$1C ;2
+    JMP SwapPRG ;3
 
     ;nop out the rest of the function
     NOP 
@@ -44,9 +46,9 @@ BtlMag_Effect_DamageUndead:
     JMP BtlMag_ApplyDamage              ; And do the actual damage!
 
 ;assembled bytes
-;20 73 B8 20 F9 B8 20 2D B8 A9 95 48 A9 03 48 A9 1B 4C 03 FE EA EA EA EA EA EA EA EA EA 60
+;20 73 B8 20 F9 B8 20 2D B8 A9 A2 48 A9 03 48 A9 1C 4C 03 FE EA EA EA EA EA EA EA EA EA 60
 
-.ORG $9500
+.ORG $A200
 lut_CharClassPtrTable:
   .byte $00
   .byte $40
@@ -55,7 +57,7 @@ lut_CharClassPtrTable:
 
 HarmHurtWmOnly:   
     LDA btlmag_defender_category
-    AND CATEGORY_UNDEAD
+    AND #CATEGORY_UNDEAD
     BNE UNDEAD
       ;zero hit chance
       LDA #0
@@ -100,5 +102,5 @@ HarmHurtWmOnly:
       LDA #$0C
       JMP SwapPRG
 
-;assembled bytes @ bank 1b 9500
-;00 40 80 C0 AD 86 68 25 08 D0 30 A9 00 8D 56 68 8D 57 68 AD 89 6C 29 7F AA BD 00 95 A8 B9 00 61 C9 04 F0 17 C9 0A F0 13 A9 00 8D 58 68 8D 59 68 A9 B9 48 A9 21 48 A9 0C 4C 03 FE A9 B9 48 A9 22 48 A9 0C 4C 03 FE
+;assembled bytes @ bank 1C A200
+;00 40 80 C0 AD 86 68 29 08 D0 30 A9 00 8D 56 68 8D 57 68 AD 89 6C 29 7F AA BD 00 A2 A8 B9 00 61 C9 04 F0 17 C9 0A F0 13 A9 00 8D 58 68 8D 59 68 A9 B9 48 A9 21 48 A9 0C 4C 03 FE A9 B9 48 A9 22 48 A9 0C 4C 03 FE
