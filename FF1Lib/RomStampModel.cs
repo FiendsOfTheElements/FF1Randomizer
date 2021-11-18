7e1fd4f037c17f51a9c8fbbb20785c98b1a83051
SaggingRufus <saggingrufus@outlook.com>
2021-11-17 23:09:16 -0400

    created RomStampModel
    RomStampModel will contain all of the information to stamp at the end of the rom for debugging purposes
       - seed
          contains the value of the seed the ROM was generated based om
       - flags
          contains and encoded string that represents the flags used during ROM generation
       - commitSha
          contains the git commit SHA of the randomizer build used to generate the ROM
       - inputRomSha
          contains the SHA of the rom that was used as input into the randomizer.

---
+++ FF1Lib/RomStampModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF1Lib
+{
+	public class RomStampModel
+	{
+		private byte[] seed = new byte[64];
+		private byte[] flags = new byte[19];
+		private byte[] commitSha = new byte[40];
+		private byte[] inputRomSha = new byte[40];
+
+		public RomStampModel (String _seed, String _flags, String _commitSha, String _inputRomSha)
+		{
+			seed = byteArrayFormatter(_seed, seed.Length);
+			flags = byteArrayFormatter(_flags, flags.Length);
+			commitSha = byteArrayFormatter(_commitSha, commitSha.Length);
+			inputRomSha = byteArrayFormatter(_inputRomSha, inputRomSha.Length);
+		}
+
+		private byte[] byteArrayFormatter(String _inputString, int _maxLenth)
+		{
+			return Encoding.ASCII.GetBytes(_inputString.PadLeft(_maxLenth, ' '));
+		}
+	}
+}
