2779464807af183a5db928287e96f0b7587ec0d8
mhn65536 <53567009+mhn65536@users.noreply.github.com>
2021-12-02 20:04:08 +0100

    kill-it (#759)
    Co-authored-by: x.y <x.y@z>

--- FF1Lib/FF1Rom.cs
+++ FF1Lib/FF1Rom.cs
@@ -955,11 +955,7 @@ namespace FF1Lib
 				ItemsText[(int)Item.House] = "XETH@p";
 			}
 
-			if (flags.ExtensiveHints_Enable)
-			{
-				new ExtensiveHints(rng, npcdata, flags, overworldMap, this).Generate();
-			}
-			else if ((bool)flags.HintsVillage || (bool)flags.HintsDungeon)
+			if ((bool)flags.HintsVillage || (bool)flags.HintsDungeon)
 			{
 				if ((bool)flags.HintsDungeon)
 					SetDungeonNPC(flippedMaps, rng);
