3de78fa57c7f88626e078d964484a9f3624a00c0
DarkTwinge <darktwinge@gmail.com>
2022-06-29 07:54:02 -0600

    Revert "Bugfix - don't use new starting-spell blursings if Spellcrafter enabled"
    This reverts commit 6507b7bdf31f5300a0283c4e4f7cc16f530a979a.

--- FF1Lib/Classes.cs
+++ FF1Lib/Classes.cs
@@ -695,12 +695,8 @@ namespace FF1Lib
 			}*/
 
 			// Single Spells Bonus/Malus
-			// Incompatible with Spellcrafter, so only add them when Spellcrafter is not enabled
-			if (!(bool)flags.GenerateNewSpellbook)
-			{
-				bonusNormal.AddRange(CreateSpellBonuses(rom, rng));
-				malusNormal.AddRange(CreateSpellMaluses(rom, rng));
-			}
+			bonusNormal.AddRange(CreateSpellBonuses(rom, rng));
+			malusNormal.AddRange(CreateSpellMaluses(rom, rng));
 
 			// Add Lockpicking Bonus/Malus
 			if ((bool)flags.Lockpicking && flags.LockpickingLevelRequirement < 50)
