a617ba1c750ec701f8de30067d2443b9855d0313
Entroper <entroper@gmail.com>
2022-07-11 01:01:47 -0400

    update Blazorizer to .NET 6

--- FF1Blazorizer/Program.cs
+++ FF1Blazorizer/Program.cs
@@ -1,30 +1,25 @@
-using System;
+global using System;
+global using System.Collections.Generic;
+global using System.Threading.Tasks;
+global using System.Text;
+
 using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
-using BlazorStrap;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+
+using BlazorStrap;
 using Blazored.LocalStorage;
 using FF1Blazorizer;
 
-namespace FF1Blazorizer
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-			builder.Services.AddBootstrapCss();
-			builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBootstrapCss();
+builder.Services.AddBlazoredLocalStorage();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
