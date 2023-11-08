bb83d32461d9669f6287711017a7ee88b13c097f
wildham0 <64616385+wildham0@users.noreply.github.com>
2023-11-08 16:25:49 -0500

    Update to 4.7.4

--- FF1Lib/FFRVersion.cs
+++ FF1Lib/FFRVersion.cs
@@ -3,7 +3,7 @@
 	// THIS FILE IS UPDATED DURING THE BUILD AND DEPLOY. DO NOT EDIT UNLESS YOU KONW WHAT YOU ARE DOING.
 	public static class FFRVersion
 	{
-		public static readonly string Version = "4.7.3";
+		public static readonly string Version = "4.7.4";
 		public static readonly string Sha = "SHA";
 		public static readonly string Branch = "BRANCH";
 		public static readonly string MasterBranch = "master";
