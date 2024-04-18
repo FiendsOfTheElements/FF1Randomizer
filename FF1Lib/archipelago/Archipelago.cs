c63943f04cf84c633230c4d612acbcce9bf80a4f
wildham0 <64616385+wildham0@users.noreply.github.com>
2024-04-18 08:12:02 -0400

    DeepDungeon AP fix

--- FF1Lib/archipelago/Archipelago.cs
+++ FF1Lib/archipelago/Archipelago.cs
@@ -162,9 +162,15 @@ namespace FF1Lib
 				options = new ArchipelagoFFROptions
 				{
 					items = logic.RewardSources.GroupBy(r => GetItemId(r.RewardSource.Item)).ToDictionary(r => GetItemName(r.First().RewardSource.Item), r => new ArchipelagoItem { id = r.Key, count = r.Count(), incentive = incentivesData.IncentiveItems.Contains(r.First().RewardSource.Item) }),
-					locations = logic.RewardSources.ToDictionary(r => apLocationNames[GetLocationId(r)], r => GetLocationId(r)),
-					locations2 = logic.RewardSources.ToDictionary(r => apLocationNames[GetLocationId(r)], r => new ArchipelagoLocation { id = GetLocationId(r), incentive = IsLocationIncentivized(r) }),
-					rules = logic.RewardSources.ToDictionary(r => apLocationNames[GetLocationId(r)], r => GetRule(r))
+					locations = logic.RewardSources.ToDictionary(
+						r => apLocationNames.TryGetValue(GetLocationId(r), out var locname) ? locname : r.RewardSource.Name,
+						r => GetLocationId(r)),
+					locations2 = logic.RewardSources.ToDictionary(
+						r => apLocationNames.TryGetValue(GetLocationId(r), out var locname) ? locname : r.RewardSource.Name,
+						r => new ArchipelagoLocation { id = GetLocationId(r), incentive = IsLocationIncentivized(r) }),
+					rules = logic.RewardSources.ToDictionary(
+						r => apLocationNames.TryGetValue(GetLocationId(r), out var locname) ? locname : r.RewardSource.Name,
+						r => GetRule(r))
 				}
 			};
 
