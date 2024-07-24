34e09163fd76aa3efffbb0e6ffd618a19fb2729c
wildham0 <64616385+wildham0@users.noreply.github.com>
2024-07-24 18:07:06 -0400

    Fix party select all flags

--- FF1Lib/Flags/FlagsParty.cs
+++ FF1Lib/Flags/FlagsParty.cs
@@ -13,7 +13,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (FIGHTER1 == FIGHTER2 == FIGHTER3 == FIGHTER4)
+				if (new[] { FIGHTER1, FIGHTER2, FIGHTER3, FIGHTER4 }.Distinct().Count() == 1)
 				{
 					return FIGHTER1;
 				}
@@ -31,7 +31,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (THIEF1 == THIEF2 == THIEF3 == THIEF4)
+				if (new[] { THIEF1, THIEF2, THIEF3, THIEF4 }.Distinct().Count() == 1)
 				{
 					return THIEF1;
 				}
@@ -49,7 +49,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (BLACK_BELT1 == BLACK_BELT2 == BLACK_BELT3 == BLACK_BELT4)
+				if (new[] { BLACK_BELT1, BLACK_BELT2, BLACK_BELT3, BLACK_BELT4 }.Distinct().Count() == 1)
 				{
 					return BLACK_BELT1;
 				}
@@ -67,7 +67,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (RED_MAGE1 == RED_MAGE2 == RED_MAGE3 == RED_MAGE4)
+				if (new[] { RED_MAGE1, RED_MAGE2, RED_MAGE3, RED_MAGE4 }.Distinct().Count() == 1)
 				{
 					return RED_MAGE1;
 				}
@@ -85,7 +85,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (WHITE_MAGE1 == WHITE_MAGE2 == WHITE_MAGE3 == WHITE_MAGE4)
+				if (new[] { WHITE_MAGE1, WHITE_MAGE2, WHITE_MAGE3, WHITE_MAGE4 }.Distinct().Count() == 1)
 				{
 					return WHITE_MAGE1;
 				}
@@ -103,7 +103,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (BLACK_MAGE1 == BLACK_MAGE2 == BLACK_MAGE3 == BLACK_MAGE4)
+				if (new[] { BLACK_MAGE1, BLACK_MAGE2, BLACK_MAGE3, BLACK_MAGE4 }.Distinct().Count() == 1)
 				{
 					return BLACK_MAGE1;
 				}
@@ -121,7 +121,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (KNIGHT1 == KNIGHT2 == KNIGHT3 == KNIGHT4)
+				if (new[] { KNIGHT1, KNIGHT2, KNIGHT3, KNIGHT4 }.Distinct().Count() == 1)
 				{
 					return KNIGHT1;
 				}
@@ -139,7 +139,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (NINJA1 == NINJA2 == NINJA3 == NINJA4)
+				if (new[] { NINJA1, NINJA2, NINJA3, NINJA4 }.Distinct().Count() == 1)
 				{
 					return NINJA1;
 				}
@@ -157,7 +157,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (MASTER1 == MASTER2 == MASTER3 == MASTER4)
+				if (new[] { MASTER1, MASTER2, MASTER3, MASTER4 }.Distinct().Count() == 1)
 				{
 					return MASTER1;
 				}
@@ -175,7 +175,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (RED_WIZ1 == RED_WIZ2 == RED_WIZ3 == RED_WIZ4)
+				if (new[] { RED_WIZ1, RED_WIZ2, RED_WIZ3, RED_WIZ4 }.Distinct().Count() == 1)
 				{
 					return RED_WIZ1;
 				}
@@ -193,7 +193,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (WHITE_WIZ1 == WHITE_WIZ2 == WHITE_WIZ3 == WHITE_WIZ4)
+				if (new[] { WHITE_WIZ1, WHITE_WIZ2, WHITE_WIZ3, WHITE_WIZ4 }.Distinct().Count() == 1)
 				{
 					return WHITE_WIZ1;
 				}
@@ -211,7 +211,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (BLACK_WIZ1 == BLACK_WIZ2 == BLACK_WIZ3 == BLACK_WIZ4)
+				if (new[] { BLACK_WIZ1, BLACK_WIZ2, BLACK_WIZ3, BLACK_WIZ4 }.Distinct().Count() == 1)
 				{
 					return BLACK_WIZ1;
 				}
@@ -229,7 +229,7 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (NONE_CLASS2 == NONE_CLASS3 == NONE_CLASS4)
+				if (new[] { NONE_CLASS2, NONE_CLASS3, NONE_CLASS4 }.Distinct().Count() == 1)
 				{
 					return NONE_CLASS2;
 				}
@@ -247,7 +247,8 @@ namespace FF1Lib
 		{
 			get
 			{
-				if (FORCED1 == FORCED2 == FORCED3 == FORCED4)
+
+				if (new[] { FORCED1, FORCED2, FORCED3, FORCED4 }.Distinct().Count() == 1)
 				{
 					return FORCED1;
 				}
