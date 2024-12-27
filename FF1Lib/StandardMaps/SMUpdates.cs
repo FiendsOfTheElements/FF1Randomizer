d8ca9763ee507b9a12fe9f0d94ae17481fcb97fe
wildham0 <64616385+wildham0@users.noreply.github.com>
2024-12-27 09:18:32 -0500

    Disable swap stairs with nooverworld

--- FF1Lib/StandardMaps/SMUpdates.cs
+++ FF1Lib/StandardMaps/SMUpdates.cs
@@ -83,7 +83,7 @@ namespace FF1Lib
 			var mapFlipper = new FlippedMaps(rom, this, flags, teleporters, rng);
 			VerticalFlippedMaps = mapFlipper.VerticalFlipStep1();
 
-			if ((bool)flags.ReversedFloors) new ReversedFloors(rom, this, rng, teleporters, VerticalFlippedMaps).Work((bool)flags.ProcgenEarth);
+			if ((bool)flags.ReversedFloors && (flags.GameMode == GameModes.Standard)) new ReversedFloors(rom, this, rng, teleporters, VerticalFlippedMaps).Work((bool)flags.ProcgenEarth);
 
 			if((bool)flags.VerticallyFlipDungeons) mapFlipper.VerticalFlipStep2();
 
