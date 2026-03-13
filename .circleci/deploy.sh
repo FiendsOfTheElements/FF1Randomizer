a5d392c4baf7909cc3e960f70a0240e4b4db5e28
Entroper <entroper@gmail.com>
2026-03-13 02:58:23 -0400

    try not using alpine

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -21,8 +21,8 @@ deployPreview=$(echo "$config" | jq -r ".deployPreview")
 #    		      --header 'Content-Type: application/json' \
 #                      --data-binary '{"type":"A", "hostname": "wiki.finalfantasyrandomizer.com", "value": "207.246.91.234"}'
 
-echo "24.13.0" > .nvmrc
-export NODE_VERSION=24.13.0
+echo "22.22.1" > .nvmrc
+export NODE_VERSION=22.22.1
 if "$deployPreview"; then
     deploy_response=$(netlify deploy --json --dir=/root/ff1randomizer/FF1Blazorizer/output/wwwroot --site="$netlifyID")
     url=$(echo "$deploy_response" | jq -r ".deploy_url")
